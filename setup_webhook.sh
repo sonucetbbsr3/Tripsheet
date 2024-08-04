66bc727cf245d6289c5d3bca93d7c749d5609b8c
sonucetbbsr3 <121528949+sonucetbbsr3@users.noreply.github.com>
2024-08-04 21:01:14 +0530

    Update setup_webhook.sh

--- setup_webhook.sh
+++ setup_webhook.sh
@@ -4,5 +4,4 @@ ngrok config add-authtoken $NGROK_AUTH_TOKEN
 ./ngrok http 5000 > /dev/null &
 sleep 10
 NGROK_URL=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r .tunnels[0].public_url)
-echo "NGROK_URL=${NGROK_URL}" > ngrok_url.txt
 curl -F "url=${NGROK_URL}/webhook" "https://api.telegram.org/bot${BOT_TOKEN}/setWebhook"
